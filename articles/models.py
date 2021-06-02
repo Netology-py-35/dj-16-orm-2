e19333abaa8a0d0eb6535a6efdb43a340fae614d
Kishinskiy Oleg <ovkishinskiy@sberbank.ru>
2021-06-02 08:10:47 +0300

    add tag class to models

--- articles/models.py
+++ articles/models.py
@@ -2,15 +2,23 @@ from django.db import models
 
 
 class Article(models.Model):
-
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение', )
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='articles')
 
     def __str__(self):
         return self.title
